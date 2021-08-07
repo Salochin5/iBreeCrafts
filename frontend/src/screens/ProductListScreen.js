16045dad118027b86bd0bdca35cbc9067386c655
Salochin <nickysalochin@gmail.com>
2021-08-07 11:54:51 +0300

    Frontend Admin Create Product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,12 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listProducts, deleteProduct } from "../actions/productActions";
+import {
+ listProducts,
+ deleteProduct,
+ createProduct,
+} from "../actions/productActions";
+import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
 
 const ProductListScreen = ({ history, match }) => {
  const dispatch = useDispatch();
@@ -19,16 +24,37 @@ const ProductListScreen = ({ history, match }) => {
   success: successDelete,
  } = productDelete;
 
+ const productCreate = useSelector((state) => state.productDelete);
+ const {
+  loading: loadingCreate,
+  error: errorCreate,
+  success: successCreate,
+  product: createdProduct,
+ } = productCreate;
+
  const userLogin = useSelector((state) => state.userLogin);
  const { userInfo } = userLogin;
 
  useEffect(() => {
-  if (userInfo && userInfo.isAdmin) {
-   dispatch(listProducts());
-  } else {
+  dispatch({ type: PRODUCT_CREATE_RESET });
+
+  if (!userInfo.isAdmin) {
    history.push("/login");
   }
- }, [dispatch, history, userInfo, successDelete]);
+
+  if (successCreate) {
+   history.push(`/admin/product/${createProduct._id}/edit`);
+  } else {
+   dispatch(listProducts());
+  }
+ }, [
+  dispatch,
+  history,
+  userInfo,
+  successDelete,
+  successCreate,
+  createdProduct,
+ ]);
 
  const deleteHandler = (id) => {
   if (window.confirm("Are you sure to Delete User")) {
@@ -36,8 +62,8 @@ const ProductListScreen = ({ history, match }) => {
   }
  };
 
- const createProductHandler = (product) => {
-  //   CREATE PRODUCT
+ const createProductHandler = () => {
+  dispatch(createProduct());
  };
 
  return (
@@ -48,12 +74,14 @@ const ProductListScreen = ({ history, match }) => {
     </Col>
     <Col className='text-right'>
      <Button className='my-3' onClick={createProductHandler}>
-      <i className='fas fa-plus'></i> CREATE PRODUCT
+      <i className='fas fa-plus'></i> CREATE A PRODUCT
      </Button>
     </Col>
    </Row>
    {loadingDelete && <Loader />}
    {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+   {loadingCreate && <Loader />}
+   {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
    {loading ? (
     <Loader />
    ) : error ? (
